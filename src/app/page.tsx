bebf5c2631b49dcc04cbedc2d8c008a05aa63eb1
okuyama <130431374+okuyama-code@users.noreply.github.com>
2024-01-20 03:28:42 +0900

    修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import EventCalendar from '@/calendar/EventCalendar'
+import EventCalendar from '@/components/EventCalendar'
 
 import { addDays, subDays } from 'date-fns'
 
@@ -9,15 +9,13 @@ import { addDays, subDays } from 'date-fns'
 export default function Home() {
   return (
     <>
-      <div >
-        <EventCalendar
-          events={[
-            { date: subDays(new Date(), 6), title: "Post video" },
-            { date: subDays(new Date(), 1), title: "Edit video" },
-            { date: addDays(new Date(), 3), title: "Code" },
-          ]}
-         />
-      </div>
+      <EventCalendar
+        events={[
+          { date: subDays(new Date(), 6), title: "Post video" },
+          { date: subDays(new Date(), 1), title: "Edit video" },
+          { date: addDays(new Date(), 3), title: "Code" },
+        ]}
+        />
     </>
   )
 }
